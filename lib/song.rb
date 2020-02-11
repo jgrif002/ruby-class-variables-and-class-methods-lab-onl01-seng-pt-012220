50310078cfbaa06dfa1a306d220b6861c8a3748a
Jesse Griffin <jesse.griffin@gmail.com>
2020-02-11 03:34:44 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,7 +2,7 @@ class Song
   attr_accessor :name, :artist, :genre 
   
   @@count = 0 
-  @@artist = []
+  
   
   def initialize(name, artist, genre)
     @name = name 
@@ -13,10 +13,6 @@ class Song
   
   def count 
     self.count
-  end 
-  
-  def artist 
-    @@artist << self.name 
   end 
   
 end 
