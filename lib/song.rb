81451fb79ed37781e532828c14c1178b3d33cec2
Jesse Griffin <jesse.griffin@gmail.com>
2020-02-11 04:04:31 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -43,7 +43,7 @@ class Song
     artist_count = {}
     @@artist.each do |artist| 
       if artist_count[artist] 
-        artist_count += 1 
+        artist_count[artist] += 1 
       else 
         artist_count = 1 
       end 
