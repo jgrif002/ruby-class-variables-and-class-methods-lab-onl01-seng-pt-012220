e33d19276ddf3df7312849917060a8117cae4086
Jesse Griffin <jesse.griffin@gmail.com>
2020-02-11 03:08:28 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,21 +1,4 @@
 class Song 
   attr_accessor  :name, :artist, :genre 
-  
-  @@count = 0 
-  
-  def initialize 
-    @@count += 1
-    @name = name
-    @artist = artist 
-    @genre = genre 
-  end 
-  
-  def self.count
-    @@count
-  end 
-  
-  def count
-    Song.count
-  end 
-  
+
 end 
