3a267ae3555ed63e83089c47c3b1b94f729bfa75
Jesse Griffin <jesse.griffin@gmail.com>
2020-02-11 04:03:13 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -38,6 +38,15 @@ class Song
     genre_count 
   end 
     
-  
-  
+    
+  def self.artist_count 
+    artist_count = {}
+    @@artist.each do |artist| 
+      if artist_count[artist] 
+        artist_count += 1 
+      else 
+        artist_count = 1 
+      end 
+    end 
+    artist_count 
 end 
